1892c0bc523777b25ddf21a1ff10e6a7c4362f43
heysaladin <didien.geonk@gmail.com>
2026-06-28 19:23:55 +0700

    zoom

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,8 +16,6 @@ export const viewport: Viewport = {
   themeColor: '#1a1a2e',
   width: 'device-width',
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
   viewportFit: 'cover',
 };
 
